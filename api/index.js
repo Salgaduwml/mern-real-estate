5889ed13175836db8e374fe8492caee8deb97b40
Malinda <salgaduwml@gmail.com>
2023-12-14 16:46:41 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -20,3 +22,4 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,17 +4,17 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      require: true,
+      required: true,
       unique: true,
     },
     password: {
       type: String,
-      require: true,
+      required: true,
     },
     email: {
       type: String,
       require: true,
-      unique: true,
+      uniqued: true,
     },
   },
   { timestamps: true }
