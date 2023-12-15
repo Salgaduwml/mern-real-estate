8ec64831b5cf917a8a01f472a91add5f899d7e55
Malinda <salgaduwml@gmail.com>
2023-12-15 18:13:06 +0530

    create signup page ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textBlack: "#2b2b2b",
+        textGray: "#3d4f58",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
