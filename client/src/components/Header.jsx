05b0f5e6e7119f82bdd7c2bbaf7c5498f5924af4
Malinda <salgaduwml@gmail.com>
2024-01-22 12:01:01 +0530

    complete header form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { AiOutlineSearch } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [searchTerm, setSearchTerm] = useState("");
   const { currentUser } = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermfromUrl = urlParams.get("searchTerm");
+    if (searchTermfromUrl) {
+      setSearchTerm(searchTermfromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -13,11 +33,16 @@ export default function Header() {
             Realtor
           </h1>
         </Link>
-        <form className="bg-slate-100 px-3 py-2 rounded flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 px-3 py-2 rounded flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <AiOutlineSearch className="text-slate-500" size={18} />
         </form>
