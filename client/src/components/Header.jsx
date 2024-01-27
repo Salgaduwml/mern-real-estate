66471a2441c1073d415aabcff653d39a28a221be
Malinda <salgaduwml@gmail.com>
2024-01-27 15:51:24 +0530

    complete nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { Search } from "./Search";
 
 export default function Header() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -26,26 +27,13 @@ export default function Header() {
   }, [location.search]);
 
   return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl px-4 py-3 mx-auto">
+    <header className="bg-white shadow-md">
+      <div className="flex justify-between items-center max-w-7xl px-5 py-3 mx-auto">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex text-slate-700">
-            Realtor
+          <h1 className="font-tenor text-red-600 text-3xl flex">
+            Realtor <span className="text-black">Gate</span>
           </h1>
         </Link>
-        <form
-          onSubmit={handleSubmit}
-          className="bg-slate-100 px-3 py-2 rounded flex items-center"
-        >
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <AiOutlineSearch className="text-slate-500" size={18} />
-        </form>
         <ul className="flex gap-6 items-center">
           <li className="hidden sm:inline text-slate-700 hover:underline">
             <Link to="/">Home</Link>
