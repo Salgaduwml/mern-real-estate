c2215e69a2eef69cafd5548917cf1271965b0ded
Malinda <salgaduwml@gmail.com>
2024-01-28 11:56:36 +0530

    make listing page responsive

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -38,10 +38,9 @@ const Listing = () => {
     };
     fetchListing();
   }, [listingId]);
-
   console.log(listing);
   return (
-    <section className="w-full min-h-[calc(100vh-64px)] bg-[#F7F7F7]">
+    <section className="w-full min-h-[calc(100vh-64px)]">
       {error ? (
         <p>Something went wrong</p>
       ) : loading ? (
@@ -62,20 +61,16 @@ const Listing = () => {
                 </SwiperSlide>
               ))}
             </Swiper>
-            <div className="max-w-6xl mx-auto w-full px-4 py-10 flex gap-12">
-              <div className="basis-2/3">
-                <p className="flex items-center text-green-800 font-semibold text-sm gap-2 mb-2">
+            <div className="max-w-7xl mx-auto w-full px-5 py-12 sm:py-20 flex flex-col sm:flex-row gap-12">
+              <div className="basis-2/3 flex flex-col items-start">
+                <p className="flex items-center text-red-600 font-semibold text-sm gap-2 mb-2">
                   <FaLocationDot />
                   {listing.address}
                 </p>
                 <h1 className="font-semibold text-2xl font-tenor capitalize">
                   {listing.name}
                 </h1>
-                <div>
-                  <div className="flex items-center gap-3 mt-6">
-                    <div className="w-3 h-3 bg-green-800 rounded-full" />
-                    <p>{listing.rent ? "For rent" : "For sale"}</p>
-                  </div>
+                <div className="mt-6">
                   {listing.offer ? (
                     <div className="flex gap-3 mt-3">
                       <span className="line-through text-lg font-bold text-gray-500">
@@ -95,29 +90,29 @@ const Listing = () => {
                   <h3 className="font-semibold">Description</h3>
                   <p className="leading-[1.6]">{listing.description}</p>
                 </div>
-                <ul className="text-green-800 text-sm font-medium flex gap-8 leading-[1] border p-4 rounded border-green-800/50">
+                <ul className="text-sm text-black/60 font-medium grid grid-cols-2 sm:grid-cols-4 gap-8 leading-[1] border p-4 px-6 rounded border-black/20">
                   <li className="flex items-center gap-3">
-                    <IoIosBed size={20} />
+                    <IoIosBed size={20} className="text-red-600" />
                     <span className="">
                       {listing.bedrooms} {""}
                       {listing.bedrooms > 1 ? "Beds" : "Bed"}
                     </span>
                   </li>
                   <li className="flex items-center gap-3">
-                    <FaBath size={18} />
+                    <FaBath size={18} className="text-red-600" />
                     <span className="">
                       {listing.bathrooms} {""}
                       {listing.bathrooms > 1 ? "Baths" : "Bath"}
                     </span>
                   </li>
                   <li className="flex items-center gap-3">
-                    <FaParking size={20} />
+                    <FaParking size={20} className="text-red-600" />
                     <span className="">
                       {listing.bathrooms ? "Parking" : "No Parking"}
                     </span>
                   </li>
                   <li className="flex items-center gap-3">
-                    <FaChair size={20} />
+                    <FaChair size={20} className="text-red-600" />
                     <span className="">
                       {listing.furnished ? "Furnished" : "Furnished"}
                     </span>
@@ -126,11 +121,9 @@ const Listing = () => {
               </div>
               <div className="basis-1/3">
                 {listing && currentUser?._id !== listing.userRef && (
-                  <div className="bg-green-700/5 rounded w-full p-4 border">
-                    <button className="bg-green-800 text-white font-tenor font-normal w-full p-2.5 rounded">
-                      Contact Landload
-                    </button>
-                  </div>
+                  <button className="bg-black text-white font-tenor font-normal w-full p-2.5 rounded hover:opacity-80">
+                    Contact Landload
+                  </button>
                 )}
               </div>
             </div>
